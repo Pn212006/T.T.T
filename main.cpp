b6158385511621bfb757f4ab8efa84481c2a9598
Pn212006 <nishilpatel200612@gmail.com>
2025-12-09 18:48:46 -0600

    rpg

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 #include <vector>
 #include <string>
 #include <limits>
-#include <cctype>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 class TicTacToe {
 private:
     vector<char> board;
     char currentPlayer;
-    bool gameOver;
     char player1Mark, player2Mark;
     string player1Type, player2Type;
     bool battleMode;
@@ -18,256 +18,220 @@ public:
     TicTacToe() {
         board = {'1','2','3','4','5','6','7','8','9'};
         currentPlayer = 'X';
-        gameOver = false;
         battleMode = false;
     }
 
-    void resetBoard() {
-        board = {'1','2','3','4','5','6','7','8','9'};
-    }
+    void resetBoard() { board = {'1','2','3','4','5','6','7','8','9'}; }
 
     void printBoard() {
         cout << "\n";
-        for (int i = 0; i < 9; i++) {
+        for(int i=0;i<9;i++){
             cout << " " << board[i] << " ";
-            if (i % 3 != 2) cout << "|";
-            if (i % 3 == 2 && i != 8) cout << "\n---+---+---\n";
+            if(i%3!=2) cout<<"|";
+            if(i%3==2 && i!=8) cout<<"\n---+---+---\n";
         }
-        cout << "\n\n";
+        cout << "\n";
     }
 
-    void mainMenu() {
-        while (true) {
-            cout << "Welcome to Tic-Tac-Toe!\n";
-            cout << "1. Regular Tic-Tac-Toe\n";
-            cout << "2. Battle Tic-Tac-Toe\n";
-            cout << "Choose a mode (1 or 2): ";
-            string choice;
-            getline(cin, choice);
-
-            if (choice == "1") {
-                battleMode = false;
-                player1Mark = 'X';
-                player2Mark = 'O';
-                play();
-                break;
-            } else if (choice == "2") {
-                battleMode = true;
-                setupBattleMode();
-                play();
-                break;
-            } else {
-                cout << "Invalid option! Try again.\n";
-            }
-        }
-    }
-
-    void setupBattleMode() {
-        cout << "\n--- Battle Tic-Tac-Toe Setup ---\n";
-        player1Mark = getCustomMark(1);
-        player1Type = getArchetype(1);
-
-        while (true) {
-            player2Mark = getCustomMark(2);
-            if (player2Mark == player1Mark) {
-                cout << "That mark is already taken by Player 1. Choose a different one.\n";
-            } else break;
-        }
-
-        player2Type = getArchetype(2);
-        cout << "\nPlayer 1: " << player1Mark << " (" << player1Type << ")\n";
-        cout << "Player 2: " << player2Mark << " (" << player2Type << ")\n\n";
+    void setupBattleMode(char pMark, char oMark, string pType, string oType){
+        battleMode = true;
+        player1Mark = pMark;
+        player2Mark = oMark;
+        player1Type = pType;
+        player2Type = oType;
         currentPlayer = player1Mark;
-    }
-
-    char getCustomMark(int playerNum) {
-        string input;
-        while (true) {
-            cout << "Player " << playerNum << ", choose your mark (A-Z, a-z, ?, !, *, ~, $, %, #): ";
-            getline(cin, input);
-            if (input.size() == 1 && (isalnum(input[0]) || string("?*!~$%#").find(input[0]) != string::npos))
-                return input[0];
-            cout << "Invalid mark! Try again.\n";
-        }
-    }
-
-    string getArchetype(int playerNum) {
-        string type;
-        while (true) {
-            cout << "Player " << playerNum << ", choose archetype (Paladin / Alchemist): ";
-            getline(cin, type);
-            for (auto &c : type) c = tolower(c);
-            if (type == "paladin" || type == "alchemist") return type;
-            cout << "Invalid archetype! Try again.\n";
-        }
     }
 
     int getPlayerMove() {
         string input;
         int move;
-        while (true) {
-            cout << "Player " << currentPlayer << ", enter your move (1-9): ";
+        while(true){
+            cout << "Player " << currentPlayer << ", enter move (1-9): ";
             cin >> input;
-            if (input.size() == 1 && isdigit(input[0])) {
-                move = input[0] - '0';
-                if (move < 1 || move > 9) {
-                    cout << "Invalid move! Out of range. Try again.\n";
-                } else if (board[move - 1] == player1Mark || board[move - 1] == player2Mark || 
-                           board[move - 1] == 'X' || board[move - 1] == 'O') {
-                    cout << "Invalid move! That square is already taken. Try again.\n";
-                } else {
-                    return move;
-                }
-            } else {
-                cout << "Invalid input! Please enter a single number between 1 and 9.\n";
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            }
+            if(input.size()==1 && isdigit(input[0])){
+                move = input[0]-'0';
+                if(move<1||move>9 || board[move-1]==player1Mark || board[move-1]==player2Mark)
+                    cout << "Invalid move!\n";
+                else return move;
+            } else cout << "Invalid input!\n";
         }
     }
 
-    void makeMove(int move) {
-        board[move - 1] = currentPlayer;
+    int getRandomMove(){
+        vector<int> moves;
+        for(int i=0;i<9;i++) if(board[i]!='X' && board[i]!='O') moves.push_back(i+1);
+        return moves[rand()%moves.size()];
     }
+
+    void makeMove(int move){ board[move-1] = currentPlayer; }
 
     bool checkWin() {
         int wins[8][3] = {
-            {0,1,2}, {3,4,5}, {6,7,8},
-            {0,3,6}, {1,4,7}, {2,5,8},
-            {0,4,8}, {2,4,6}
+            {0,1,2},{3,4,5},{6,7,8},
+            {0,3,6},{1,4,7},{2,5,8},
+            {0,4,8},{2,4,6}
         };
-        for (auto &win : wins) {
-            if (board[win[0]] == currentPlayer &&
-                board[win[1]] == currentPlayer &&
-                board[win[2]] == currentPlayer) {
+        for(auto &w:wins)
+            if(board[w[0]]==currentPlayer && board[w[1]]==currentPlayer && board[w[2]]==currentPlayer)
                 return true;
-            }
-        }
         return false;
     }
 
-    bool checkTie() {
-        for (char c : board) {
-            if (c != player1Mark && c != player2Mark && c != 'X' && c != 'O') return false;
-        }
+    bool checkTie(){
+        for(char c:board) if(c!=player1Mark && c!=player2Mark) return false;
         return true;
     }
 
-    void switchPlayer() {
-        if (battleMode) {
-            currentPlayer = (currentPlayer == player1Mark) ? player2Mark : player1Mark;
-        } else {
-            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-        }
-    }
+    void switchPlayer() { currentPlayer = (currentPlayer==player1Mark)?player2Mark:player1Mark; }
 
-    void alchemistSwap() {
-        string a, b;
-        cout << "Enter two positions (1-9) to swap:\n";
-        cin >> a >> b;
-        if (a.size() == 1 && b.size() == 1 && isdigit(a[0]) && isdigit(b[0])) {
-            int p1 = a[0] - '0';
-            int p2 = b[0] - '0';
-            if (p1 < 1 || p1 > 9 || p2 < 1 || p2 > 9) {
-                cout << "Invalid positions.\n"; return;
-            }
-            if (board[p1-1] == board[p2-1]) {
-                cout << "Cannot swap identical marks.\n"; return;
-            }
-            swap(board[p1-1], board[p2-1]);
-        } else {
-            cout << "Invalid input.\n";
-        }
-    }
-
-    void paladinShift() {
-        string from, to;
-        cout << "Enter mark to move and target space:\n";
-        cin >> from >> to;
-        if (from.size() == 1 && to.size() == 1 && isdigit(from[0]) && isdigit(to[0])) {
-            int src = from[0]-'0', dst = to[0]-'0';
-            if (src < 1 || src > 9 || dst < 1 || dst > 9) {
-                cout << "Invalid positions.\n"; return;
-            }
-            if (board[dst-1] == player1Mark || board[dst-1] == player2Mark) {
-                cout << "Destination occupied.\n"; return;
-            }
-            if (!isAdjacent(src, dst)) {
-                cout << "Can only move to adjacent squares.\n"; return;
-            }
-            swap(board[src-1], board[dst-1]);
-        } else {
-            cout << "Invalid input.\n";
-        }
-    }
-
-    bool isAdjacent(int a, int b) {
-        int rowA = (a-1)/3, colA = (a-1)%3;
-        int rowB = (b-1)/3, colB = (b-1)%3;
-        return (abs(rowA-rowB) <= 1 && abs(colA-colB) <= 1 && a != b);
-    }
-
-    bool checkGameResult() {
-        if (checkWin()) {
-            printBoard();
-            cout << "Player " << currentPlayer << " wins!\n";
-            return true;
-        } else if (checkTie()) {
-            printBoard();
-            cout << "It's a tie!\n";
-            return true;
-        }
-        return false;
-    }
-
-    void play() {
+    
+    bool battleRound(int &playerHealth, int playerAttack, int playerDefense,
+                     int &opponentHealth, int opponentAttack, int opponentDefense){
         resetBoard();
-        gameOver = false;
-        if (battleMode) currentPlayer = player1Mark;
-        while (!gameOver) {
+        currentPlayer = player1Mark;
+        while(true){
             printBoard();
-            if (battleMode) {
-                string type = (currentPlayer == player1Mark) ? player1Type : player2Type;
-                string choice;
-                while (true) {
-                    cout << "Player " << currentPlayer << " (" << type << ") choose:\n";
-                    cout << "1. Regular move\n2. Use special ability\n> ";
-                    cin >> choice;
-                    if (choice == "1" || choice == "2") break;
-                    cout << "Invalid input! Please enter 1 or 2.\n";
-                    cin.clear();
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                }
-                if (choice == "2") {
-                    if (type == "alchemist") alchemistSwap();
-                    else if (type == "paladin") paladinShift();
+            int move = (currentPlayer==player1Mark)? getPlayerMove() : getRandomMove();
+            makeMove(move);
+            if(checkWin()){
+                if(currentPlayer==player1Mark){
+                    int dmg = max(0, playerAttack-opponentDefense);
+                    opponentHealth -= dmg;
+                    cout << "You won this round! Opponent lost " << dmg << " health.\n";
                 } else {
-                    int move = getPlayerMove();
-                    makeMove(move);
+                    int dmg = max(0, opponentAttack-playerDefense);
+                    playerHealth -= dmg;
+                    cout << "Opponent won this round! You lost " << dmg << " health.\n";
                 }
-            } else {
-                int move = getPlayerMove();
-                makeMove(move);
+                return (playerHealth>0 && opponentHealth>0);
+            } else if(checkTie()){
+                cout << "Round tied! No damage.\n";
+                return true;
             }
-            if (checkGameResult()) {
-                gameOver = true;
-            } else {
-                switchPlayer();
-            }
+            switchPlayer();
         }
-        string again;
-        cout << "Play again? (y/n): ";
-        cin >> again;
-        if (again == "y" || again == "Y") {
-            mainMenu();
-        } else {
-            cout << "Thanks for playing!\n";
+    }
+
+    
+    void playPvP() {
+        resetBoard();
+        currentPlayer = 'X';
+        while(true){
+            printBoard();
+            int move = getPlayerMove();
+            makeMove(move);
+            if(checkWin()){
+                printBoard();
+                cout << "Player " << currentPlayer << " wins!\n";
+                break;
+            } else if(checkTie()){
+                printBoard();
+                cout << "It's a tie!\n";
+                break;
+            }
+            currentPlayer = (currentPlayer=='X')?'O':'X';
         }
     }
 };
 
-int main() {
+
+struct Character {
+    string name;
+    string type;
+    int health;
+    int attack;
+    int defense;
+    int gold;
+};
+
+void event(Character &player){
+    cout << "\n--- Event ---\n";
+    cout << "You find a potion or a training dummy.\n";
+    cout << "1. Drink potion (+20 health)\n2. Train (+5 attack, +5 defense)\n> ";
+    int choice;
+    cin >> choice;
+    if(choice==1){
+        player.health += 20;
+        cout << "Health is now " << player.health << "\n";
+    } else {
+        player.attack += 5;
+        player.defense += 5;
+        cout << "Attack: " << player.attack << ", Defense: " << player.defense << "\n";
+    }
+}
+
+
+void campaign(){
+    srand(time(0));
+    Character player;
+    cout << "Enter your hero's name: ";
+    cin >> player.name;
+    cout << "Choose class (Paladin/Alchemist): ";
+    cin >> player.type;
+    player.health = 100;
+    player.attack = (player.type=="Paladin")? 15:12;
+    player.defense = (player.type=="Paladin")? 10:8;
+    player.gold = 50;
+
+    vector<Character> foes = {
+        {"Goblin","Alchemist",40,8,5,0},
+        {"Orc","Paladin",50,12,8,0},
+        {"Dark Knight","Paladin",60,15,10,0},
+        {"Sorcerer","Alchemist",55,14,9,0},
+        {"Dragon","Paladin",100,20,12,0} // final boss
+    };
+
+    for(int i=0;i<foes.size();i++){
+        Character &foe = foes[i];
+        cout << "\n--- Battle " << i+1 << ": " << foe.name << " ---\n";
+        TicTacToe battle;
+        battle.setupBattleMode('X','O',player.type,foe.type);
+        while(player.health>0 && foe.health>0)
+            battle.battleRound(player.health, player.attack, player.defense,
+                               foe.health, foe.attack, foe.defense);
+
+        if(player.health<=0){
+            cout << "You have been defeated! Campaign restarts.\n";
+            return;
+        } else {
+            cout << "You defeated " << foe.name << "!\n";
+            player.gold += 20;
+            if(i<foes.size()-1) event(player);
+        }
+    }
+    cout << "\nCongratulations " << player.name << "! You completed the campaign!\n";
+}
+
+
+int main(){
+    srand(time(0));
     TicTacToe game;
-    game.mainMenu();
+
+    while(true){
+        cout << "\n=== Tic-Tac-Toe Menu ===\n";
+        cout << "1. Player vs Player\n";
+        cout << "2. Battle Tic-Tac-Toe (PvP with classes)\n";
+        cout << "3. Campaign/RPG Mode\n";
+        cout << "4. Exit\n";
+        cout << "Choose an option: ";
+        int choice;
+        cin >> choice;
+
+        if(choice==1){
+            game.playPvP();
+        } else if(choice==2){
+            string p1Type, p2Type;
+            char p1Mark, p2Mark;
+            cout << "Player 1, choose mark: "; cin >> p1Mark;
+            cout << "Player 1, choose class (Paladin/Alchemist): "; cin >> p1Type;
+            cout << "Player 2, choose mark: "; cin >> p2Mark;
+            cout << "Player 2, choose class (Paladin/Alchemist): "; cin >> p2Type;
+            game.setupBattleMode(p1Mark, p2Mark, p1Type, p2Type);
+            game.playPvP();
+        } else if(choice==3){
+            campaign();
+        } else break;
+    }
+    cout << "Thanks for playing!\n";
     return 0;
 }
